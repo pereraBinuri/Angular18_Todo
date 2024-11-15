b1dca98599de5abc4257cf6b07cd81fe44ff454b
Binuri Perera <pererabinuri29@gmail.com>
2024-11-15 18:35:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  searchQuery: string = ''; // Property to store the search query
+  isOpen = false; // Boolean to track sidebar open/close state
+
+  @Output() searchQueryChange = new EventEmitter<string>(); // Emit search query changes
+
+  // Method to toggle the sidebar
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { HeaderComponent } from './component/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
